a15cbaaa76379543ea2b16655a1b28782a784421
Евгений Хваль <khvaleugene@MacBook-Air-von-Evgenij.local>
2025-08-04 21:16:27 +0300

    Подключение компонента sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 import Header from "./Components/Header/header";
+import Sidebar from "./Components/Sidebar/sidebar";
 
 function App() {
   return (
     <div className="grcaApp">
       <Header />
+      <Sidebar />
     </div>
   );
 }
---
+++ frontend/src/Components/Sidebar/sidebar.js
@@ -0,0 +1,20 @@
+import "./sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-title">
+        Доступные модули:
+        <span></span>
+      </div>
+      <span></span>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
